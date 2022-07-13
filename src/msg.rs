cdbe2c7774d95fb8a56e5d8879f1075f4187f1a4
cuonglm22 <cuonglm22@fpt.com.vn>
2022-07-13 11:12:00 +0700

    update certificates

--- src/msg.rs
+++ src/msg.rs
@@ -1,27 +1,29 @@
+use cosmwasm_std::Addr;
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub owner: Addr,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     // GetCount returns the current count as a json-encoded number
-    GetCount {},
+    // GetCount {},
+    GetTokenUri { id: String },
 }
 
 // We define a custom struct for each query response
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct CountResponse {
-    pub count: i32,
+pub struct TokenUriResponse {
+    // pub count: i32,
+    uri: String
 }
--- src/state.rs
+++ src/state.rs
@@ -4,10 +4,12 @@ use serde::{Deserialize, Serialize};
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
 
+static STORE_KEY: &[u8] = b"store";
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct State {
-    pub count: i32,
+pub struct Certificate {
     pub owner: Addr,
+    pub id: String
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const CERTIFICATE: Item<Certificate> = Item::new("state");
